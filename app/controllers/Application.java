4c17ed4f9488c6092c7da5146f26936028a2570d
Lars Kristian Dahl <lars.dahl@gmail.com>
2012-10-17 16:24:28 +0200

    Fixed a bug in the index() action.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,13 @@ package controllers;
 
 import models.Contact;
 import models.Producer;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.application.index;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(index.render(new Producer(123, new Contact(null, null, null, null))));
+		return ok(views.html.application.index.render(new Producer(123, new Contact(null, null, null, null))));
 	}
 	
 	public static Result settings() {
