e0f27006d874b98ca3fb2a22883d622db73443ba
Lars Kristian Dahl <lars.dahl@gmail.com>
2012-10-23 16:42:54 +0200

    Implemented Sign in and Sign out behaviour.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,65 +1,85 @@
 package controllers;
 
 import models.Login;
+import play.Routes;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.application.*;
 
 public class Application extends Controller {
-	
+
 	public static Result index() {
-//		return ok(views.html.application.login.render("Sheep Watch"));
-		return TODO;
+		if (session("username") != null) {
+			return redirect(routes.Application.app());
+		}
+		return redirect(routes.Application.login());
 	}
-	
+
 	public static Result app() {
+		if (session("username") == null) {
+			return redirect(routes.Application.index());
+		}
 		return ok(index.render(null));
 	}
-	
+
 	public static Result settings() {
 		return ok(settings.render("settings"));
 	}
-	
 
 	public static Result login() {
+		if (session("username") != null) {
+			return redirect(routes.Application.app());
+		}
 		return ok(login.render(form(Login.class)));
 	}
-	
+
 	public static Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if(loginForm.hasErrors()) {
-            return badRequest(login.render(loginForm));
-        } else {
-            session("email", loginForm.get().username);
-            return redirect(
-                routes.Application.help()
-            );
-        }
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+		} else {
+			session("username", loginForm.get().username);
+			return redirect(routes.Application.index());
+		}
 	}
 
 	public static Result logout() {
-		return TODO;
+		session().clear();
+		flash("succes", "You have been logged out.");
+		return redirect(routes.Application.login());
 	}
-	
+
 	public static Result saveContacts() {
 		return TODO;
 	}
-	
+
 	public static Result search(String term) {
 		return TODO;
 	}
-	
+
 	public static Result help() {
 		return TODO;
 	}
-	
+
 	public static Result about() {
 		return TODO;
 	}
-	
+
 	public static Result contact() {
 		return TODO;
+	}
+
+	public static Result javascriptRoutes() {
+		response().setContentType("text/javascript");
+		return ok(Routes.javascriptRouter("jsRoutes",
+
+				// Routes for Sheep
+				controllers.routes.javascript.Sheep.index(),
+				controllers.routes.javascript.Sheep.add(),
+				controllers.routes.javascript.Sheep.delete()
+
+		));
 	}
 
 }
